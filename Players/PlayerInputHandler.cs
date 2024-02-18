fa14c3475537bb0d9a329efb2a2b1453b695c8aa
Jacob <me@here.com>
2024-02-18 17:45:57 -0500

    Working on input manager.

--- Players/PlayerInputHandler.cs
+++ Players/PlayerInputHandler.cs
@@ -1,34 +1,140 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace Players;
 
 public readonly struct InputNames
 {
-    public const string JUMP = "jump";
-    public const string LEFT = "left";
-    public const string RIGHT = "right";
-    public const string ACTION = "action";
+    public readonly static StringName JUMP = "jump";
+    public readonly static StringName LEFT = "left";
+    public readonly static StringName RIGHT = "right";
+    public readonly static StringName ACTION = "action";
+    public readonly static StringName LEFT_KB_ID = "_kb_l";
+    public readonly static StringName RIGHT_KB_ID = "_kb_r";
+}
+
+public enum DeviceType
+{
+    Gamepad,
+    KeyboardRight,
+    KeyboardLeft
+}
+
+public readonly struct InputDevice
+{
+    public readonly DeviceType Type;
+    public readonly int DeviceID;
+
+    public InputDevice(DeviceType type, int deviceID)
+    {
+        Type = type;
+        DeviceID = deviceID;
+    }
+}
+
+public static class InputDeviceExtensions
+{
+    public static bool IsEventForDevice(this InputDevice device, InputEvent @event)
+    {
+        if (@event.Device != device.DeviceID) return false;
+        else if (@event is InputEventKey key)
+        {
+            return device.Type == GetInputDeviceType(key);
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public static DeviceType GetInputDeviceType(this InputEventKey key)
+    {
+        return (key.IsAction(InputNames.LEFT + InputNames.LEFT_KB_ID)
+                || key.IsAction(InputNames.RIGHT + InputNames.LEFT_KB_ID)
+                || key.IsAction(InputNames.JUMP + InputNames.LEFT_KB_ID)
+                || key.IsAction(InputNames.ACTION + InputNames.LEFT_KB_ID))
+                ? DeviceType.KeyboardLeft : DeviceType.KeyboardRight;
+    }
+
+    public static StringName ConvertInputName(this StringName name, DeviceType type)
+    {
+        switch(type)
+        {
+            case DeviceType.KeyboardLeft:
+                return name + InputNames.LEFT_KB_ID;
+            case DeviceType.KeyboardRight:
+                return name + InputNames.RIGHT_KB_ID;
+            default:
+                return name;
+        }
+    }
 }
 
 public partial class PlayerInputHandler : Node
 {
-    int _deviceID;
+    #region Static
+    private static readonly List<InputDevice> RegisteredDevices = new();
+
+    public static InputDevice GetNextOpenDevice()
+    {
+
+    }
+    #endregion
+
+    InputDevice _device;
+
+    #region Signals
+    [Signal] public delegate void MoveLeftEventHandler();
+    [Signal] public delegate void MoveRightEventHandler();
+    [Signal] public delegate void JumpEventHandler();
+    [Signal] public delegate void ActionEventHandler();
+    #endregion
 
     public override void _Ready()
     {
         base._Ready();
+        _device = GetNextOpenDevice();
     }
 
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (@event.Device != _deviceID) return;
+        if (_device.IsEventForDevice(@event))
+        {
+            GD.Print(@event.Device, _device.Type);
+        }
+        else return;
+        //foreach (var device in Input.GetConnectedJoypads())
+        //{
+        //    GD.Print(Input.GetJoyName(device), device);
+        //}
+
+        if (@event.IsActionPressed(InputNames.LEFT.ConvertInputName(_device.Type)))
+        {
+            EmitSignal(SignalName.MoveLeft);
+            GD.Print($"{_device}: Left Input");
+        }
+        if (@event.IsActionPressed(InputNames.RIGHT.ConvertInputName(_device.Type)))
+        {
+            EmitSignal(SignalName.MoveRight);
+            GD.Print($"{_device}: Right Input");
+        }
+        if (@event.IsActionPressed(InputNames.JUMP.ConvertInputName(_device.Type)))
+        {
+            EmitSignal(SignalName.Jump);
+            GD.Print($"{_device}: Jump Input");
+        }
+        if (@event.IsActionPressed(InputNames.ACTION.ConvertInputName(_device.Type)))
+        {
+            EmitSignal(SignalName.Action);
+            GD.Print($"{_device}: Action Input");
+        }
     }
 
-    public void SetDeviceID(int deviceID)
+    public void SetDevice(InputDevice device)
     {
-        GD.Print($"Setting {Name} device ID to {deviceID}.");
-        _deviceID = deviceID;
+        GD.Print($"Setting {Name} device to {device}.");
+        _device = device;
     }
 }
