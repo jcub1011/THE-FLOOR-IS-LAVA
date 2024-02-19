8d1bacbefee1c2c33786e795bb9064b13c40c43c
Jacob <me@here.com>
2024-02-18 21:24:55 -0500

    Added horizontal movement to player.

--- Players/HorizontalMovementHandler.cs
+++ Players/HorizontalMovementHandler.cs
@@ -7,4 +7,28 @@ public partial class HorizontalMovementHandler : Node
 {
     [Export] PlayerController _body;
     [Export] float _moveSpeed;
+    bool _isLeftButtonDown;
+    bool _isRightButtonDown;
+
+    public void OnLeftPressed() => _isLeftButtonDown = true;
+    public void OnLeftReleased() => _isLeftButtonDown = false;
+    public void OnRightPressed() => _isRightButtonDown = true;
+    public void OnRightReleased() => _isRightButtonDown = false;
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        Vector2 newVel = new(0, _body.Velocity.Y);
+
+        if (_isLeftButtonDown)
+        {
+            newVel.X += -_moveSpeed;
+        }
+        if (_isRightButtonDown)
+        {
+            newVel.X += _moveSpeed;
+        }
+
+        _body.Velocity = newVel;
+    }
 }
