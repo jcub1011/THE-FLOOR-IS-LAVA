81c18a8a723099209f45f1977469a4bd8943a0dc
Jacob <me@here.com>
2024-02-21 20:42:47 -0500

    Added unfinished implmentation of player spawner.

---
+++ Levels/PlayerSpawner.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorldGeneration;
+
+public partial class PlayerSpawner : Node2D
+{
+    #region Static
+    static List<Vector2> _spawnLocations;
+    public static List<Vector2> SpawnLocations
+    {
+        get => _spawnLocations.ToList();
+    }
+    #endregion
+
+    public override void _Ready()
+    {
+        base._Ready();
+        var spawnLocs = new List<Vector2>();
+
+        foreach(var child in GetChildren())
+        {
+            if (spawnLocs.Count == 4) break;
+            else if (child is Marker2D marker)
+            {
+                spawnLocs.Add(marker.GlobalPosition);
+            }
+        }
+        _spawnLocations = spawnLocs;
+        if (_spawnLocations.Count < 4) GD.PushError($"Insufficient spawn " +
+            $"locations. Make sure to add spawn locations via " +
+            $"Marker2D.");
+    }
+}
