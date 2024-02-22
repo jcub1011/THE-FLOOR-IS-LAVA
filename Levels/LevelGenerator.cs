bba0297f3a151850878fa397c36640c4ef2cb4ea
Jacob <me@here.com>
2024-02-21 21:35:35 -0500

    Added function to delete section when it goes below the world.

--- Levels/LevelGenerator.cs
+++ Levels/LevelGenerator.cs
@@ -5,5 +5,32 @@ namespace WorldGeneration;
 
 public partial class LevelGenerator : Node2D
 {
+    [Export] float _startDelay = 5f;
+    [Export] Camera2D _camera;
 
+    public float? _worldBottomY;
+    public float WorldBottomY
+    {
+        get
+        {
+            if (_worldBottomY == null) return float.PositiveInfinity;
+            else return _worldBottomY.Value;
+        }
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _worldBottomY = GetWorldBottomY();
+    }
+
+    float GetWorldBottomY()
+    {
+        float cameraPos = _camera.GetScreenCenterPosition().Y;
+        var cameraRect =
+            GetCanvasTransform().AffineInverse().BasisXform(GetViewportRect().Size);
+        float returnVal = cameraPos + cameraRect.Y / 2;
+        GD.Print($"World bottom = {returnVal}");
+        return returnVal;
+    }
 }
---
+++ Levels/WorldSection.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+namespace WorldGeneration;
+
+public partial class WorldSection : CharacterBody2D
+{
+    [Export] Marker2D _sectionUpperBound;
+    [Export] Marker2D _sectionLowerBound;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Velocity = new(0f, 50f);
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        MoveAndSlide();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        if (IsInstanceValid(this) && !IsVisible())
+        {
+            QueueFree();
+        }
+    }
+
+    float GetWorldBottomY()
+    {
+        return GetParent<LevelGenerator>().WorldBottomY;
+    }
+
+    public bool IsVisible()
+    {
+        return _sectionUpperBound.GlobalPosition.Y < GetWorldBottomY();
+    }
+}
