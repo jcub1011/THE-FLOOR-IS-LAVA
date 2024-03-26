4cd58229eab5936b5bfdf97bf6b8dc78f02e3524
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2024-03-26 16:27:21 -0400

    Working on pause screen implementation.

--- UI/PauseScreen.cs
+++ UI/PauseScreen.cs
@@ -4,6 +4,25 @@ using System;
 
 namespace UI;
 
+public static class PauseEventChannel
+{
+    /// <summary>
+    /// The bool parameter is true when pause is enabled and false when pause is disabled.
+    /// </summary>
+    public static event Action<bool> OnPauseEvent;
+
+    /// <summary>
+    /// This value is not updated until after the OnPauseEvent is invoked.
+    /// </summary>
+    public static bool IsPaused { get; private set; }
+
+    public static void SetPauseState(bool paused)
+    {
+        OnPauseEvent?.Invoke(paused);
+        IsPaused = paused;
+    }
+}
+
 public partial class PauseScreen : Control
 {
     [Signal] public delegate void OnRestartPressedEventHandler();
@@ -13,10 +32,14 @@ public partial class PauseScreen : Control
     [Export] StringName _restartText = "Restart";
     [Export] StringName _mainMenuText = "Main Menu";
     [Export] StringName _resumeText = "Resume";
+    [Export] StringName _pauseInputName = "pause";
+
+    bool _enabled = false;
 
     public override void _Ready()
     {
         base._Ready();
+        SetIfEnabled(_enabled);
 
         var buttons = this.GetChildren<Button>();
         foreach(var button in buttons)
@@ -34,6 +57,26 @@ public partial class PauseScreen : Control
                 button.Pressed += OnResumeButtonPressed;
             }
         }
+        PauseEventChannel.OnPauseEvent += OnPauseCallback;
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        PauseEventChannel.OnPauseEvent -= OnPauseCallback;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (@event.IsActionPressed(_pauseInputName)) PauseEventChannel.SetPauseState(!PauseEventChannel.IsPaused);
+    }
+
+    void OnPauseCallback(bool paused)
+    {
+        if (_enabled)
+            Visible = paused;
+        else Visible = false;
     }
 
     void OnRestartButtonPressed()
@@ -46,11 +89,26 @@ public partial class PauseScreen : Control
     {
         GD.Print("Main menu pressed.");
         EmitSignal(SignalName.OnMainMenuPressed);
+        Visible = false;
     }
 
     void OnResumeButtonPressed()
     {
         GD.Print("Resume pressed.");
         EmitSignal(SignalName.OnResumePressed);
+        PauseEventChannel.SetPauseState(false);
+    }
+
+    public void SetIfEnabled(bool isEnabled)
+    {
+        _enabled = isEnabled;
+        if (isEnabled)
+        {
+            Visible = PauseEventChannel.IsPaused;
+        }
+        else
+        {
+            Visible = false;
+        }
     }
 }
