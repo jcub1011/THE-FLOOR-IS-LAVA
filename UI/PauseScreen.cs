d96c88fe2884901c6547b0387e8e60e7bc17db63
Jacob McCormack <91000914+jcub1011@users.noreply.github.com>
2024-03-26 11:27:08 -0400

    Connected button signals for pause screen.

--- UI/PauseScreen.cs
+++ UI/PauseScreen.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.NodeExtensions;
 using System;
 
 namespace UI;
@@ -16,5 +17,40 @@ public partial class PauseScreen : Control
     public override void _Ready()
     {
         base._Ready();
+
+        var buttons = this.GetChildren<Button>();
+        foreach(var button in buttons)
+        {
+            if (button.Text == _restartText)
+            {
+                button.Pressed += OnRestartButtonPressed;
+            }
+            else if (button.Text == _mainMenuText)
+            {
+                button.Pressed += OnMainMenuButtonPressed;
+            }
+            else if (button.Text == _resumeText)
+            {
+                button.Pressed += OnResumeButtonPressed;
+            }
+        }
+    }
+
+    void OnRestartButtonPressed()
+    {
+        GD.Print("Restart pressed.");
+        EmitSignal(SignalName.OnRestartPressed);
+    }
+
+    void OnMainMenuButtonPressed()
+    {
+        GD.Print("Main menu pressed.");
+        EmitSignal(SignalName.OnMainMenuPressed);
+    }
+
+    void OnResumeButtonPressed()
+    {
+        GD.Print("Resume pressed.");
+        EmitSignal(SignalName.OnResumePressed);
     }
 }
